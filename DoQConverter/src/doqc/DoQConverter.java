72ec4c9892bf1587b4d08d7ec5e400c20a961129
Peter <siposp@gmail.com>
2014-02-15 23:59:43 +0100

    Added some testing stuff

--- DoQConverter/src/doqc/DoQConverter.java
+++ DoQConverter/src/doqc/DoQConverter.java
@@ -1,5 +1,27 @@
 package doqc;
 
 public class DoQConverter {
-
+	private int test_i=0;
+	
+	public DoQConverter(){
+		this.test_i=-1;
+	}
+	
+	public void beta(){
+		this.test_i++;
+	}
+	
+	public int info(){
+		return this.test_i;
+	}
+	
+	
+	public static void main(String[] args){
+		DoQConverter doqc=new DoQConverter();
+		doqc.beta();
+		doqc.beta();
+		doqc.beta();
+		
+		System.out.println(doqc.info());
+	}
 }
